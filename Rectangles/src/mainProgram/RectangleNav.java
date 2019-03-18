0ed2ac79c74ff056adfab6b8f46086cfc3d098ab
jgayn <jgayn@129.12.170.64>
2019-03-18 20:13:41 +0000

    Setup

---
+++ Rectangles/src/mainProgram/RectangleNav.java
@@ -0,0 +1,15 @@
+package mainProgram;
+
+import java.util.List;
+
+import tools.*;
+
+public class RectangleNav {
+	public static void main(String args[]) {
+
+	}
+
+	public List<Vertex> nextConfigs(Vertex state) {
+
+	}
+}
